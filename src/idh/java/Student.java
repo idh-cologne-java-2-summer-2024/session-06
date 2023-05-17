83841f287eafbefd5c612390897ff1773233af13
Nils Reiter <nils.reiter@uni-koeln.de>
2023-05-17 13:46:49 +0200

    Presentation in class

--- src/idh/java/Student.java
+++ src/idh/java/Student.java
@@ -1,7 +1,10 @@
 package idh.java;
 
+import java.util.HashSet;
+
 public class Student extends Object {
 	String name;
+	int matrikelNummer;
 
 	public String getName() {
 		return name;
@@ -11,18 +14,45 @@ public class Student extends Object {
 		this.name = name;
 	}
 
-	public Student(String name) {
+	public Student(String name, int mNum) {
 		super();
 		this.name = name;
+		this.matrikelNummer = mNum;
 	}
 
 	public String toString() {
 		return name;
 	}
 
+	public boolean equals(Object o) {
+		if (! (o instanceof Student))
+			return false;
+		Student s = (Student) o;
+		return this.matrikelNummer == s.matrikelNummer;
+	}
+	
+	public int hashCode() {
+		return matrikelNummer;
+	}
+	
 	public static void main(String[] args) {
-		Student s1 = new Student("Maria Müller");
-		Student s2 = new Student("Stefan Schmidt");
-		Student s3 = new Student("Maria Müller");
+		Student s1 = new Student("Maria Müller", 123);
+		Student s2 = new Student("Stefan Schmidt", 234);
+		Student s3 = new Student("Maria Müller", 123);
+		
+		System.out.println("MARIO".equalsIgnoreCase("mario"));
+		System.out.println(s1.equals(s2));
+		System.out.println(s1.equals(s3));
+		System.out.println(s1 == s2);
+		System.out.println(s1 == s3);
+		System.out.println(5 == 3);
+		System.out.println(5 == 5);
+		
+		HashSet<Student> studentGroup = new HashSet<Student>();
+		studentGroup.add(s1);
+		studentGroup.add(s2);
+		studentGroup.add(s3);
+		System.out.println(studentGroup);
+		
 	}
 }
